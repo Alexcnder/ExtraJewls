9c0860b7243ee01082031f0db8bd1da0698aaf06
Alexcnder <87881787+Alexcnder@users.noreply.github.com>
2025-01-08 21:58:36 +0100

    Create Jade.kt

---
+++ src/main/kotlin/com/theonlytails/rubymod/items/Jade.kt
@@ -0,0 +1,56 @@
+package com.theonlytails.rubymod.items
+
+import com.theonlytails.rubymod.entities.RubySheepEntity
+import com.theonlytails.rubymod.registries.EntityTypeRegistry
+import com.theonlytails.rubymod.rubyTabProperty
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.SpawnReason
+import net.minecraft.entity.passive.SheepEntity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.util.ActionResultType
+import net.minecraft.util.Hand
+import net.minecraft.world.IServerWorld
+
+/**
+ * Holds the custom functionality of rubies.
+ *
+ * @author TheOnlyTails
+ */
+class opal : Item(opalTabProperty) {
+	override fun interactLivingEntity(
+		stack: ItemStack,
+		playerIn: PlayerEntity,
+		target: LivingEntity,
+		hand: Hand,
+	): ActionResultType =
+		if (target is SheepEntity && target.isAlive && !target.isSheared && target !is opalSheepEntity) {
+			if (!playerIn.level.isClientSide) {
+				EntityTypeRegistry.opalSheep.create(playerIn.level)?.apply {
+					moveTo(
+						target.x,
+						target.y,
+						target.z,
+						target.yRot,
+						target.xRot
+					)
+
+					finalizeSpawn(
+						playerIn.level as IServerWorld,
+						playerIn.level.getCurrentDifficultyAt(blockPosition()),
+						SpawnReason.CONVERSION,
+						null,
+						null
+					)
+
+					playerIn.level.addFreshEntity(this)
+					target.remove()
+
+					stack.shrink(1)
+				}
+			}
+
+			ActionResultType.sidedSuccess(playerIn.level.isClientSide)
+		} else ActionResultType.PASS
+}
